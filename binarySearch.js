786d283766c0707bc1d2f0a870961dd34ca90229
hector3211 <hectororopesa5@gmail.com>
2022-09-18 01:47:21 -0400

    second commit

--- binarySearch.js
+++ binarySearch.js
@@ -6,17 +6,17 @@ function binarySearchFunction(haystack, needle) {
   // [1,2,3,4,5,6,7,8,9,10]
   let hi = haystack.length; // = 10
   while (lo < hi) {
-    const m = Math.floor(lo + (hi - lo) / 2); // = 5 from our list
-    const v = haystack[m]; // this will be index 5 , number 6
+    const middle = Math.floor(lo + (hi - lo) / 2); // = 5 from our list
+    const v = haystack[middle]; // this will be index 5 , number 6
     if (v === needle) {
       // if index 5,number 6 = needle return true
       return console.log(true);
     } else if (v > needle) {
       // if number 6 Greater than needle
-      hi = m; // we 1/2 our search and hi becomes index 5 instead of 10
+      hi = middle; // we 1/2 our search and hi becomes index 5 instead of 10
     } else {
       // if number 6 less than needle
-      lo = m + 1; // our lo variable becomes index 6,number 7
+      lo = middle + 1; // our lo variable becomes index 6,number 7
     }
   }
   return console.log(false);
